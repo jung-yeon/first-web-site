08e64a29013c935b548946ba654da5f1aeb0ce20
이정연 <99dlwjddus@naver.com>
2023-12-28 20:33:56 +0900

    javaScript제어문 추가, 홈페이지 제작

--- main.js
+++ main.js
@@ -1,15 +1,94 @@
 var http = require("http");
 var fs = require("fs");
+var url = require("url");
 var app = http.createServer(function (request, response) {
-  var url = request.url;
-  if (request.url == "/") {
-    url = "/index.html";
+  var _url = request.url;
+  //url을 분석하는 코드
+  var queryData = url.parse(_url, true).query;
+  var pathname = url.parse(_url, true).pathname;
+
+  if (pathname === "/") {
+    if (queryData.id === undefined) {
+      var title = "Welcome";
+      var description = "Hello, Node.js";
+      var template = `<!DOCTYPE html>
+        <html>
+          <head>
+            <title>WEB1-html</title>
+            <meta charset="utf-8" />
+            <link rel="stylesheet" href="style.css" />
+          </head>
+          <body>
+            <h1>
+              <a href="/"><font color="lightblue">WEB</font></a>
+            </h1>
+            <div id="grid">
+              <ol>
+                <li>
+                  <a href="/?id=HTML"><font color="blue">HTML</font></a>
+                </li>
+                <li>
+                  <a href="/?id=CSS"><font color="blue">CSS</font></a>
+                </li>
+                <li>
+                  <a href="/?id=JavaScript"><font color="blue">JavaScript</font></a>
+                </li>
+              </ol>
+              <div id="article">
+                <h2>${title}</h2>
+                <p>
+                  ${description}
+                </p>
+              </div>
+            </div>
+          </body>
+        </html>
+        `;
+      response.writeHead(200);
+      response.end(template);
+    } else {
+      var title = queryData.id;
+      fs.readFile(`data/${queryData.id}`, "utf8", function (err, description) {
+        var template = `<!DOCTYPE html>
+        <html>
+          <head>
+            <title>WEB1-html</title>
+            <meta charset="utf-8" />
+            <link rel="stylesheet" href="style.css" />
+          </head>
+          <body>
+            <h1>
+              <a href="/"><font color="lightblue">WEB</font></a>
+            </h1>
+            <div id="grid">
+              <ol>
+                <li>
+                  <a href="/?id=HTML"><font color="blue">HTML</font></a>
+                </li>
+                <li>
+                  <a href="/?id=CSS"><font color="blue">CSS</font></a>
+                </li>
+                <li>
+                  <a href="/?id=JavaScript"><font color="blue">JavaScript</font></a>
+                </li>
+              </ol>
+              <div id="article">
+                <h2>${title}</h2>
+                <p>
+                  ${description}
+                </p>
+              </div>
+            </div>
+          </body>
+        </html>
+        `;
+        response.writeHead(200);
+        response.end(template);
+      });
+    }
+  } else {
+    response.writeHead(404);
+    response.end("Not found");
   }
-  if (request.url == "/favicon.ico") {
-    return response.writeHead(404);
-  }
-  response.writeHead(200);
-  console.log(__dirname + url);
-  response.end(fs.readFileSync(__dirname + url));
 });
 app.listen(3000);
---
+++ syntax/String.js
@@ -0,0 +1,5 @@
+console.log("1" + "1");
+console.log(
+  "As the golden sun dipped below the horizon, a serene silence enveloped the countryside. Fields of golden wheat swayed gently in the evening breeze, and a distant row of mountains stood silhouetted against the fading twilight sky. In this tranquil moment, the hustle and bustle of the day melted away, leaving a sense of peace and simplicity. It was a time to reflect on the day's events, to appreciate the beauty of the natural world, and to look forward to the promise of a new day."
+    .length
+);
